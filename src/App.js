35a97290a18fe328411e4f07f700107ed918e674
longdaobk17 <ripad3011@gmail.com>
2020-03-14 20:24:56 +0700

    Last update

--- src/App.js
+++ src/App.js
@@ -10,15 +10,45 @@ function App() {
 }
 
 const PersonList = () => {
+
+  const people = [
+    {
+      id: 23,
+      name: "Steve",
+      job: "Engirneer"
+    },
+    {
+      id: 46,
+      name: "Hoan Miler",
+      job: "Doctor"
+    },
+    {
+      id: 64,
+      name: "Michel James",
+      job: "Dancer"
+    }
+  ]
+
   return (
-    <Person></Person>
+    <>
+      <Person person = {people[0]}></Person>
+      <Person person = {people[1]}></Person>
+      <Person person = {people[2]}></Person>
+    </>
     );
   }
   
-const Person = () => {
+const Person = (props) => {
+  const person = props.person;
+  const path = "https://randomuser.me/api/portraits/thumb/men/";
+  const url = path + person.id + ".jpg";
   return(
-    <div>
-      I'm funtion....
+    <div className="person">
+      <img src={url} alt="some person" />
+      <div>
+        <span>Name: </span><h4>{person.name}</h4>
+        <span>Job: </span><h4>{person.job}</h4>
+      </div>
     </div>
   )
 }
